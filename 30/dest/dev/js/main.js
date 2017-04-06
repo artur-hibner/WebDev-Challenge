4c575e568de1199fef73897efd819232345f72e0
artur-hibner <artur@hiweb.pl>
2017-04-06 20:16:48 +0200

    some effects

--- 30/dest/dev/js/main.js
+++ 30/dest/dev/js/main.js
@@ -1,2 +1,21 @@
+$(document).ready(function(){
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 200) {
+			$('#returnToTop').fadeIn(200);
+			$('#nav').addClass('navScroll');
+		} else {
+			$('#returnToTop').fadeOut(200);
+			$('#nav').removeClass('navScroll');
 
+		}
+	});
+
+	$('#returnToTop').click(function(){
+		$('body,html').animate({
+			scrollTop: 0
+		}, 1000);
+	});
+
+
+});
 //# sourceMappingURL=main.js.map
