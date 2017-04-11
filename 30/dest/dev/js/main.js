2d84857830915306918d4ffd8362cbd4d088b66e
artur-hibner <artur@hiweb.pl>
2017-04-11 15:44:28 +0200

    animated hamburger menu

--- 30/dest/dev/js/main.js
+++ 30/dest/dev/js/main.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
+	$('nav a').click(function(event){
+		var id = $(this).attr('href'),
+			offset = 66,
+			target = $(id).offset().top - offset;
+			$('html body').animate({
+				scrollTop: target
+			}, 1000);
+			event.preventDefault();
+	});
+
 	$(window).scroll(function(){
 		if ($(this).scrollTop() > 200) {
 			$('#returnToTop').fadeIn(200);
@@ -6,8 +16,26 @@ $(document).ready(function(){
 		} else {
 			$('#returnToTop').fadeOut(200);
 			$('#nav').removeClass('navScroll');
-
 		}
+	});
+
+	$('#mobile-toggle').on('click', function(){
+		$(this).toggleClass('mobile-toggle--open');
+	});
+
+	$('.mobile-toggle').click(function(){
+		if ($('#nav').hasClass('nav--open')) {
+			$('#nav').removeClass('nav--open');
+		} else {
+			$('#nav').addClass('nav--open');
+		}
+	});
+
+	$('.nav li a').click(function(){
+		if ($('#nav').hasClass('nav--open')) {
+			$('#nav').removeClass('nav--open')
+			$('#mobile-toggle').removeClass('mobile-toggle--open');
+		} 
 	});
 
 	$('#returnToTop').click(function(){
@@ -15,7 +43,6 @@ $(document).ready(function(){
 			scrollTop: 0
 		}, 1000);
 	});
-
 
 });
 //# sourceMappingURL=main.js.map
