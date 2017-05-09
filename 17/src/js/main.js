0b6744f510d32c546ebcbc1f65e26deea397fd20
artur-hibner <artur@hiweb.pl>
2017-05-09 15:49:27 +0200

    add mobile menu

---
+++ 17/src/js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+	$('#mobile-nav-toggle').on('click', function(){
+		$(this).toggleClass('mobile-nav-toggle--open');
+	});
+
+	var nav = '.page-header__nav .wrapper ul',
+		navLink = '.page-header__nav .wrapper ul li a',
+		navMobile = '#mobile-nav-toggle';
+		
+	$('.mobile-nav-toggle').click(function(){
+		if ($(nav).hasClass('nav--open')) {
+			$(nav).removeClass('nav--open');
+		} else {
+			$(nav).addClass('nav--open');
+		}
+	});
+
+	$(navLink).click(function(){
+		if ($(nav).hasClass('nav--open')) {
+			$(nav).removeClass('nav--open')
+			$(navMobile).removeClass('mobile-nav-toggle--open');
+		} 
+	});
+
+
+
+});
